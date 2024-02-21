2d4de271f9a1082d9c7539e299f3ad26f1dbbfc7
Alexey Kivilev <alexkivi10@yandex.ru>
2024-02-21 10:04:40 +0500

    correct issues 1

--- src/test/java/AlexTheLionTest.java
+++ src/test/java/AlexTheLionTest.java
@@ -3,40 +3,34 @@ import com.example.AlexTheLion;
 import com.example.Feline;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Spy;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AlexTheLionTest {
-
-    @Spy
+    @Mock
     Feline feline;
-
-    AlexTheLion alex = new AlexTheLion(feline, "Самец");
-
-    public AlexTheLionTest() throws Exception {
-    }
-
+    
     @Test
-    public void checkOnFriends() {
+    public void checkOnFriends() throws Exception {
         List<String> expected = List.of("Зебра Марти", "Бегемотиха Глория", "Жираф Мелман");
-        List<String> actual =  alex.getFriends();
+        List<String> actual =  new AlexTheLion(feline, "Самец").getFriends();
         assertEquals("Ожидаемый результат не соответствует актуальному", expected, actual);
     }
 
     @Test
-    public void placeOfLivingShouldBeNewYorkZoo() {
+    public void placeOfLivingShouldBeNewYorkZoo() throws Exception {
         String expected = "Нью-Йорский зоопарк";
-        String actual = alex.getPlaceOfLiving();
+        String actual = new AlexTheLion(feline, "Самец").getPlaceOfLiving();
         assertEquals("Ожидаемый результат не соответствует актуальному", expected, actual);
     }
 
     @Test
-    public void maleLionHasNoKittens() {
+    public void maleLionHasNoKittens() throws Exception {
         int expected = 0;
-        int actual = alex.getKittens();
+        int actual = new AlexTheLion(feline, "Самец").getKittens();
         assertEquals("Ожидаемый результат не соответствует актуальному", expected, actual);
     }
 
